ad3ac3bf1b8dcc87188a6d3c5c55e21d9ea06a31
Brandon Mortensen <btmorty@gmail.com>
2017-03-31 23:47:15 -0600

    Add Albums and Photos pages and coppied list vew from ustoria project

--- Photos.aspx.cs
+++ Photos.aspx.cs
@@ -5,15 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class client : System.Web.UI.Page
+public partial class Photos : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
     }
 
-    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
-    {
-
-    }
 }
